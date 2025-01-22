ffbb85775654b3b3c443fa0a2d89651a11f30a81
ub-victor <victoireushindi371@gmail.com>
2025-01-22 10:10:34 +0200

    Correct bags in this website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+/** @type {import('tailwindcss').Config} */  
+module.exports = {  
+  content: ['./index.html'], 
+  theme: {  
+    extend: {},  
+  },  
+  plugins: [],  
+};
